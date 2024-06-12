bc389c5b7d6e291a1df64f13a5c0d65503720175
nitinkumaar20 <nk999549@gmail.com>
2024-06-12 14:25:32 +0530

    test

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,23 +1,14 @@
-
-import { motion } from "framer-motion"
-
+import { motion } from "framer-motion";
 
 const AboutMe = () => {
   const variants = {
-    visible:{x:0},
-    hidden:{x:100},
-    }
+    visible: { x: 0 },
+    hidden: { x: 100 },
+  };
   return (
     <section className="about-sec " id="section-2">
-      <motion.div 
-      className="section-heading"
-   
-      >
-     
-        <ul
-    
-        >
-
+      <motion.div className="section-heading">
+        <ul>
           <li>A</li>
           <li>B</li>
           <li>O</li>
@@ -25,44 +16,77 @@ const AboutMe = () => {
           <li>T</li>
           <li>M</li>
           <li>E</li>
-
         </ul>
-  
       </motion.div>
 
-{/* /////////////////////////image and text /////////////////////////////////////////////////  */}
-
-        <div className='row about-div' >
-
-        <div className="col-md-6 img-dev"  >
+      {/* ///////////////////image and text ///////////////////////////  */}
+      <div className="row about-div">
+        <div className="col-md-6 img-dev">
           <img
-          
             src="https://drive.google.com/thumbnail?id=1ZbOzj2R3IVdOyg2xRxTHP9AcATYRF7bz&sz=w1000"
-       
             alt="Error"
-            />
+          />
         </div>
-        <div className="col-md-6 text-div" >
-          <motion.div className="py-md-5 px-0 " initial="hidden" variants={variants} whileInView="visible" viewport={{once:true}} transition={{duration:.5}} >
+        <div className="col-md-6 text-div">
+          <motion.div
+            className="py-md-5 px-0 "
+            initial="hidden"
+            variants={variants}
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ duration: 0.5 }}
+          >
+            <motion.h3
+              initial="hidden"
+              variants={variants}
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.7 }}
+            >
+              LET’S
+            </motion.h3>
 
-          <motion.h3 initial="hidden" variants={variants} whileInView="visible" viewport={{once:true}} transition={{duration:.7}}  >LET’S</motion.h3>
+            <motion.h1
+              initial="hidden"
+              variants={variants}
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.5 }}
+            >
+              INTRODUCE ABOUT
+            </motion.h1>
 
-  <motion.h1 initial="hidden" variants={variants} whileInView="visible" viewport={{once:true}} transition={{duration:.5}} >
-  INTRODUCE ABOUT</motion.h1>
+            <motion.h3
+              initial="hidden"
+              className="h3-MYSELF"
+              variants={variants}
+              whileInView="visible"
+              viewport={{ once: true }}
+              transition={{ duration: 0.3 }}
+            >
+              MYSELF
+            </motion.h3>
 
-  <motion.h3 initial="hidden" className="h3-MYSELF" variants={variants} whileInView="visible" viewport={{once:true}} transition={{duration:.3}}  >
-  MYSELF</motion.h3>
+            <h6 className="h6-1">
+              I am Nitin Kumar a Web Developer and Graphic Designer, I completed
+              my B.Tech in Computer Science Engineering from the Phonics Group
+              of Institutions.
+            </h6>
 
-        <h6 className="h6-1" >I am Nitin Kumar a Web Developer and Graphic Designer, I completed my B.Tech in Computer Science Engineering from the Phonics Group of Institutions.</h6>
-
-        <h6 className="h6-2"  >I have Experience in <span style={{color:'#10537a'}} > React, Node, MongoDB, JavaScript, HTML, CSS, and Bootstrap and PhotoShop.</span> And I love to create beautiful and efficient websites for my customers. I love going through the entire process with the customer from concept, to design and then development and launch.</h6>
-
-      
-  
+            <h6 className="h6-2">
+              I have Experience in{" "}
+              <span style={{ color: "#10537a" }}>
+                {" "}
+                React, Node, MongoDB, JavaScript, HTML, CSS, and Bootstrap and
+                PhotoShop.
+              </span>{" "}
+              And I love to create beautiful and efficient websites for my
+              customers. I love going through the entire process with the
+              customer from concept, to design and then development and launch.
+            </h6>
           </motion.div>
         </div>
-        
-            </div>
+      </div>
     </section>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,43 +1,75 @@
-import React from "react";
-import { useRef,useState } from "react";
+import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { motion } from "framer-motion"
 
 const Navbar = () => {
-//   const navRef = useRef();
   const [addCls, setAddCls] = useState(false);
 
   const showNavbar = () => {
-    setAddCls((state)=>!state);
-    // navRef.current.classList.toggle(`responsive_nav`);
+    setAddCls((state) => !state);
+    // this is test comment
   };
 
   return (
     <section>
-    <header >
-      {/* <h3>LOGO</h3> */}
-      <div id="logodiv" >
-                <a href="#">
-                  
-                    <img src=" https://drive.google.com/thumbnail?id=1IaAN_QyzOWGfOlVmk33P4Evu5W-jeVj_" alt="error"/>
-                   
-                </a>
-            </div>
-      <button className="nav-btn" onClick={showNavbar}>
-        <FaBars />
-      </button>
-      {/* #section-one */}
-      <nav className={`${addCls?'responsive_nav':''}`}>
-        <a href="#section-one" onClick={()=>{setAddCls((e)=>!e)}}>Home</a>
-        <a href="#section-2" onClick={()=>{setAddCls((e)=>!e)}}>About</a>
-        <a href="#section-3" onClick={()=>{setAddCls((e)=>!e)}}>Services</a>
-        <a href="#portfolio" onClick={()=>{setAddCls((e)=>!e)}}>Portfolio</a>
-        <a href="#contact" onClick={()=>{setAddCls((e)=>!e)}}>Contact</a>
-        <button className="nav-btn nav-close-btn" onClick={showNavbar}>
-          <FaTimes />
+      <header>
+        <div id="logodiv">
+          <a href="#">
+            <img
+              src=" https://drive.google.com/thumbnail?id=1IaAN_QyzOWGfOlVmk33P4Evu5W-jeVj_"
+              alt="error"
+            />
+          </a>
+        </div>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
         </button>
-      </nav>
-    </header>
+        {/* #section-one */}
+        <nav className={`${addCls ? "responsive_nav" : ""}`}>
+          <a
+            href="#section-one"
+            onClick={() => {
+              setAddCls((e) => !e);
+            }}
+          >
+            Home
+          </a>
+          <a
+            href="#section-2"
+            onClick={() => {
+              setAddCls((e) => !e);
+            }}
+          >
+            About
+          </a>
+          <a
+            href="#section-3"
+            onClick={() => {
+              setAddCls((e) => !e);
+            }}
+          >
+            Services
+          </a>
+          <a
+            href="#portfolio"
+            onClick={() => {
+              setAddCls((e) => !e);
+            }}
+          >
+            Portfolio
+          </a>
+          <a
+            href="#contact"
+            onClick={() => {
+              setAddCls((e) => !e);
+            }}
+          >
+            Contact
+          </a>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+      </header>
     </section>
   );
 };
